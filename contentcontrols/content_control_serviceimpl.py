cb7e819f4a806baf602a95f37d88892192acbc30
BetaBWJaguarr <tunarasimocak@gmail.com>
2026-03-04 02:56:50 +0300

    Update

--- contentcontrols/content_control_serviceimpl.py
+++ contentcontrols/content_control_serviceimpl.py
@@ -1,11 +1,13 @@
-from typing import Optional
-
+from typing import Optional, Dict
 from contentcontrols.content_control_service import ContentControlService
 from contentcontrols.content_control import ContentDecision, ContentControlEngine
 from user.workspace import Workspace
 
 
 class ContentControlServiceImpl(ContentControlService):
+
+    def __init__(self):
+        self._engines: Dict[str, ContentControlEngine] = {}
 
     def evaluate_content(
             self,
@@ -15,7 +17,20 @@ class ContentControlServiceImpl(ContentControlService):
             user_role: Optional[str] = None
     ) -> ContentDecision:
 
-        engine = ContentControlEngine(workspace)
+        workspace_id = str(workspace.id)
+
+        if workspace_id not in self._engines:
+            self._engines[workspace_id] = ContentControlEngine(workspace)
+
+        engine = self._engines[workspace_id]
+
+
+        if engine.workspace.updated_at != workspace.updated_at:
+            self._engines[workspace_id] = ContentControlEngine(workspace)
+            engine = self._engines[workspace_id]
+
+        if not user_identifier:
+            user_identifier = "anonymous"
 
         return engine.evaluate(
             user_id=user_identifier,
--- contentcontrols/utils/contentcontrolsettings.py
+++ contentcontrols/utils/contentcontrolsettings.py
@@ -10,7 +10,12 @@ class SpamSettings:
     duplicate_check: bool = True
     duplicate_reset_seconds: int = 30
     burst_limit: int = 20
+    burst_window_seconds: int = 60
     cooldown_seconds: int = 60
+    exempt_roles: List[str] = field(default_factory=list)
+    max_message_length: int = 1000
+    max_emojis: int = 20
+    max_repeated_char: int = 10
 
 
 @dataclass
