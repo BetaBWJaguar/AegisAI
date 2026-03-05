170d9333a2697810d48e88b19c4a459b0ac597c0
BetaBWJaguarr <tunarasimocak@gmail.com>
2026-03-06 01:20:41 +0300

    Update

--- contentcontrols/content_control_serviceimpl.py
+++ contentcontrols/content_control_serviceimpl.py
@@ -1,4 +1,6 @@
+import time
 from typing import Optional, Dict
+
 from contentcontrols.content_control_service import ContentControlService
 from contentcontrols.content_control import ContentDecision, ContentControlEngine
 from user.workspace import Workspace
@@ -6,8 +8,21 @@ from user.workspace import Workspace
 
 class ContentControlServiceImpl(ContentControlService):
 
+    ENGINE_TTL_SECONDS = 600
+
     def __init__(self):
-        self._engines: Dict[str, ContentControlEngine] = {}
+        self._engines: Dict[str, dict] = {}
+
+    def _cleanup_cache(self):
+        now = time.time()
+
+        expired = [
+            wid for wid, data in self._engines.items()
+            if now - data["created_at"] > self.ENGINE_TTL_SECONDS
+        ]
+
+        for wid in expired:
+            del self._engines[wid]
 
     def evaluate_content(
             self,
@@ -17,17 +32,25 @@ class ContentControlServiceImpl(ContentControlService):
             user_role: Optional[str] = None
     ) -> ContentDecision:
 
+        self._cleanup_cache()
+
         workspace_id = str(workspace.id)
 
         if workspace_id not in self._engines:
-            self._engines[workspace_id] = ContentControlEngine(workspace)
+            self._engines[workspace_id] = {
+                "engine": ContentControlEngine(workspace),
+                "created_at": time.time()
+            }
 
-        engine = self._engines[workspace_id]
+        cache_entry = self._engines[workspace_id]
+        engine: ContentControlEngine = cache_entry["engine"]
 
-
-        if engine.workspace.updated_at != workspace.updated_at:
-            self._engines[workspace_id] = ContentControlEngine(workspace)
-            engine = self._engines[workspace_id]
+        if engine.workspace.updated_at < workspace.updated_at:
+            self._engines[workspace_id] = {
+                "engine": ContentControlEngine(workspace),
+                "created_at": time.time()
+            }
+            engine = self._engines[workspace_id]["engine"]
 
         if not user_identifier:
             user_identifier = "anonymous"
